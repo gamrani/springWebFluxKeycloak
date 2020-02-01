1e7daab1be72e624c1ab01f052874868fbd06e72
gamraniyoussef <gamraniyoussef@gmail.com>
2020-02-01 13:26:05 +0100

    add roles

--- src/main/java/com/example/library/server/business/BookService.java
+++ src/main/java/com/example/library/server/business/BookService.java
@@ -4,6 +4,7 @@ import com.example.library.server.dataaccess.Book;
 import com.example.library.server.dataaccess.BookRepository;
 import com.example.library.server.dataaccess.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 @Service
+@PreAuthorize("hasAnyRole('LIBRARY_USER', 'LIBRARY_CURATOR')")
 public class BookService {
 
   private final BookRepository bookRepository;
@@ -22,6 +24,7 @@ public class BookService {
     this.userRepository = userRepository;
   }
 
+  @PreAuthorize("hasRole('LIBRARY_CURATOR')")
   public Mono<Void> create(Mono<Book> book) {
     return bookRepository.insert(book).then();
   }
@@ -30,6 +33,7 @@ public class BookService {
     return bookRepository.findById(uuid);
   }
 
+  @PreAuthorize("hasRole('LIBRARY_USER')")
   public Mono<Void> borrowById(UUID bookIdentifier, UUID userIdentifier) {
 
     if (bookIdentifier == null || userIdentifier == null) {
@@ -78,6 +82,7 @@ public class BookService {
     return bookRepository.findAll();
   }
 
+  @PreAuthorize("hasRole('LIBRARY_CURATOR')")
   public Mono<Void> deleteById(UUID bookIdentifier) {
     return bookRepository.deleteById(bookIdentifier).then();
   }
--- src/main/java/com/example/library/server/config/WebSecurityConfiguration.java
+++ src/main/java/com/example/library/server/config/WebSecurityConfiguration.java
@@ -1,15 +1,59 @@
 package com.example.library.server.config;
 
+import com.example.library.server.common.Role;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+import java.net.URI;
 
 @EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class WebSecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+        return serverHttpSecurity.authorizeExchange()
+                .pathMatchers(HttpMethod.POST, "/books/{bookId}/borrow")
+                .hasRole(Role.LIBRARY_USER.name())
+                .pathMatchers(HttpMethod.POST, "/books/{bookId}/return")
+                .hasRole(Role.LIBRARY_USER.name())
+                .pathMatchers(HttpMethod.POST, "/books")
+                .hasRole(Role.LIBRARY_CURATOR.name())
+                .pathMatchers(HttpMethod.DELETE, "/books")
+                .hasRole(Role.LIBRARY_CURATOR.name())
+                .pathMatchers("/users/**")
+                .hasRole(Role.LIBRARY_ADMIN.name())
+                .anyExchange()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutSuccessHandler(logoutSuccessHandler())
+                .and()
+                .build();
+    }
+    @Bean
+    public ServerLogoutSuccessHandler logoutSuccessHandler() {
+        RedirectServerLogoutSuccessHandler logoutSuccessHandler = new RedirectServerLogoutSuccessHandler();
+        logoutSuccessHandler.setLogoutSuccessUrl(URI.create("/books"));
+        return logoutSuccessHandler;
+    }
+
 }
